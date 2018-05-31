c3140034b92a4857a27c1c1d31e563defd18116f
Thijs Brobbel <t.brobbel@coolblue.nl>
2018-05-31 20:44:42 +0200

    Add CLI.

---
+++ YnabImporter.Cli/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+using YnabImporter.Core;
+
+namespace YnabImporter.Cli
+{
+    internal static class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: ynabimporter [RABOBANK CSV FILE] [YNAB CSV FILE]");
+
+                return 2;
+            }
+
+            var rabobankCsvFile = args[0];
+            var ynabCsvFile = args[1];
+
+            var rabobankCsvText = File.ReadAllText(rabobankCsvFile);
+
+            var ynabCsvText = YnabCsvImporter.FromRabobank(rabobankCsvText);
+
+            File.WriteAllText(ynabCsvFile, ynabCsvText);
+
+            return 0;
+        }
+    }
+}
