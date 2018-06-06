ccdeede60ebf69dd33cd1c4926f68ec97c1ca2c8
Thijs Brobbel <t.brobbel@coolblue.nl>
2018-06-06 22:03:10 +0200

    Add API endpoint for converting Rabobank CSV to Ynab.

---
+++ YnabImporter.Api/Controllers/YnabImporterController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using YnabImporter.Core;
+
+namespace YnabImporter.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class YnabImporterController : Controller
+    {
+        [HttpPost]
+        public string Post([FromBody]string rabobankCsvText)
+        {
+            return YnabCsvImporter.FromRabobank(rabobankCsvText);
+        }
+    }
+}
