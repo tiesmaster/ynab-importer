3f5cd595216e6a5adae7c3481742045d12e832c7
Thijs Brobbel <thijsbrobbel@gmail.com>
2018-06-07 21:29:55 +0200

    wip

--- ynab-importer/src/App.tsx
+++ ynab-importer/src/App.tsx
@@ -1,19 +1,64 @@
-import * as React from 'react';
-import './App.css';
+import * as React from "react";
+import "./App.css";
 
-import logo from './logo.svg';
+import { ChangeEvent, MouseEvent } from "react";
 
 class App extends React.Component {
+  constructor(props: any) {
+    super(props);
+    this.handleFileSelected = this.handleFileSelected.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  public handleFileSelected(e: ChangeEvent<HTMLInputElement>) {
+    // tslint:disable-next-line:no-console
+    console.log(e.currentTarget.value);
+    // tslint:disable-next-line:no-console
+    console.log(e.currentTarget.files);
+    if (e.currentTarget.files != null) {
+      const f = e.currentTarget.files[0];
+      const reader = new FileReader();
+      const name = f.name;
+      // tslint:disable-next-line:no-console
+      console.log("File name: " + name);
+
+      reader.onload = (fr: FileReaderProgressEvent) => {
+        const target: any = fr.target;
+        const data = target.result;
+
+        // tslint:disable-next-line:no-console
+        console.log(data);
+
+        fetch("http://localhost:55856/api/YnabImporter", {
+          body: JSON.stringify(data),
+          headers: {
+            "Content-Type": "application/json-patch+json"
+          },
+          method: "POST"
+        }).then(response => {
+          // tslint:disable-next-line:no-console
+          console.log(response);
+          // tslint:disable-next-line:no-console
+          console.log(response.json());
+        });
+      };
+      reader.readAsText(f);
+    }
+
+    e.preventDefault();
+  }
+  public handleClick(e: MouseEvent<HTMLButtonElement>) {
+    // tslint:disable-next-line:no-console
+    console.log("Hoi");
+    e.preventDefault();
+  }
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <form>
+          <label htmlFor="file">Select Rabobank CSV</label>
+          <input type="file" id="file" onChange={this.handleFileSelected} />
+          <button onClick={this.handleClick}>Convert to YNAB CSV</button>
+        </form>
       </div>
     );
   }
